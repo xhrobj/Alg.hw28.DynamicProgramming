8f491691266681d14689ca2c8992e6b6fa657b8a
M <m1@mishe1.ru>
2022-06-28 21:53:17 +0300

    Some improve

--- dp.playground/Contents.swift
+++ dp.playground/Contents.swift
@@ -13,14 +13,12 @@ func maxPathSum(for tree: xmasTree) -> Int {
 
     guard height > 1 else { return tree[0][0] }
 
-    var tree = tree, levelIndex = height - 2
+    var (tree, levelIndex) = (tree, height - 2)
 
     while levelIndex >= 0 {
         let level = tree[levelIndex]
         for (i, x) in level.enumerated() {
-            let x1 = x + tree[levelIndex + 1][i]
-            let x2 = x + tree[levelIndex + 1][i + 1]
-            tree[levelIndex][i] = x1 > x2 ? x1 : x2
+            tree[levelIndex][i] = max(x + tree[levelIndex + 1][i], x + tree[levelIndex + 1][i + 1])
         }
         levelIndex -= 1
     }
